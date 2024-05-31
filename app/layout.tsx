ebe9bd19ba54591ac472c7930c2e345c5eded8ba
Thiago Vieira <thiagoasvieira@gmail.com>
2024-05-31 12:53:25 -0300

    Put information at the Head

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "TODOAPP",
+  description: "The fastest way to register your tasks",
 };
 
 export default function RootLayout({
